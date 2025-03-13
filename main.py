316ac51d1c4c88cac30b87d10ad6d4e59c0a8ec9
Sapir-Bar <sapirbar22@gmail.com>
2025-03-13 11:28:47 +0200

    update 13/03

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
 from data_loader_asnicarF import load_gene_families_data_from_csv
+from data_loader_asnicarF import process_big_data
 
 if __name__ == "__main__":
 
    input_path = "data/data_files/raw_data/2021-03-31.AsnicarF_2021_gene_families.csv"
-   output_dir = "data/data_files/gene_families/trial/"
-   load_gene_families_data_from_csv(input_path, output_dir)
+   output_dir = "trial-2021"
+   process_big_data(input_path, output_dir)
