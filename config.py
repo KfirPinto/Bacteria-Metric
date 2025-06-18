bf6b8551dc46db1cc386a5572a58551247f4838d
nogazit99 <noga.zahav@gmail.com>
2025-06-18 14:12:42 +0300

    commit changes

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 # Paths to data files
-TENSOR_PATH = "data/PRJEB53403_168_samples/genefamilies/after_intersection/tensor.npy"
+GENE_FAMILIES_TENSOR_PATH = "data/PRJEB53403_168_samples/genefamilies/after_intersection/tensor.npy"
+PATHWAYS_TENSOR_PATH = "data/PRJEB53403_168_samples/pathabundance/after_intersection/tensor.npy"
 SAMPLE_LIST_PATH = "data/PRJEB53403_168_samples/genefamilies/after_intersection/sample_list.npy"
 BACTERIA_LIST_PATH = "data/PRJEB53403_168_samples/genefamilies/after_intersection/bacteria_list.npy"
+UNANNOTATED_BACTERIA_LIST_PATH = "data/PRJEB53403_168_samples/genefamilies/after_intersection/unannotated_bacteria_list.npy"
 GENE_LIST_PATH = "data/PRJEB53403_168_samples/genefamilies/after_intersection/gene_families_list.npy"
+PATHWAYS_LIST_PATH = "data/PRJEB53403_168_samples/pathabundance/after_intersection/pathway_list.npy"
 
 # Output directory for processed data
-NAME = "03/06/2025"
-SERIAL_NUM = 10
+NAME = "09/06/2025"
+SERIAL_NUM = 17
 EVAL_OUTPUT_DIR = f"eval_data/Run_{SERIAL_NUM}"
 
 # Split ratios
@@ -17,7 +20,8 @@ VAL_RATIO = 0.15
 BATCH_SIZE = 64
 NUM_EPOCHS = 100
 LEARNING_RATE = 0.001
-EMBEDDING_DIM = 128
+EMBEDDING_DIM = 32
+
 λ1 = 4.45652E-07
 λ2 = 0.964183454
 λ3 = 0.035816101
