5a51a8f18b5a29b7783c1ba7270e6084f9c77e19
nogazit99 <noga.zahav@gmail.com>
2025-06-22 15:24:47 +0300

    code reorganization

--- checkDim.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-def checkDim(data):
-    pathway_data = np.load(data, allow_pickle=True)
-    print(f"Loaded data shape: {pathway_data.shape}")
-
-if __name__ == "__main__":
-    
-    checkDim("/home/bcrlab/barsapi1/metric/Bacteria-Metric/data/PRJEB53403_168_samples/genefamilies/before_intersection/bacteria_list.npy")
-    checkDim("/home/bcrlab/barsapi1/metric/Bacteria-Metric/data/PRJEB53403_168_samples/genefamilies/after_intersection/bacteria_list.npy")
