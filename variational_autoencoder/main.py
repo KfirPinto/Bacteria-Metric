eb37582dc30e1990575ce10550c98b51f7a2cf83
Sapir-Bar <sapirbar22@gmail.com>
2025-11-06 13:45:11 +0200

    updated to 1 training

--- variational_autoencoder/main.py
+++ variational_autoencoder/main.py
@@ -37,62 +37,60 @@ def main():
     data_tensor = torch.tensor(data_norm, dtype=torch.float32)
     unannotated_data_tensor = torch.tensor(unannotated_data_norm, dtype=torch.float32)
 
-    for i in range (10):
+    out_dir = f"{config.EVAL_OUTPUT_DIR}"
+    os.makedirs(out_dir, exist_ok=True)
 
-        out_dir = f"{config.EVAL_OUTPUT_DIR}/Run_{i}/"
-        os.makedirs(out_dir, exist_ok=True)
+    # Shuffle and split
+    data_tendor_shuffled, bacteria_shuffled = shuffle_bacteria(data_tensor, bacteria)
+    split = split_tensor(data_tendor_shuffled, bacteria_shuffled, unannotated_data_tensor, unannotated_bacteria, config.TRAIN_RATIO)
 
-        # Shuffle and split
-        data_tendor_shuffled, bacteria_shuffled = shuffle_bacteria(data_tensor, bacteria)
-        split = split_tensor(data_tendor_shuffled, bacteria_shuffled, unannotated_data_tensor, unannotated_bacteria, config.TRAIN_RATIO)
+    # Save evaluation sets
+    save_eval_data(split, samples, gene_families, out_dir=out_dir)
 
-        # Save evaluation sets
-        save_eval_data(split, samples, gene_families, out_dir=out_dir)
+    # Create DataLoaders
+    train_loader = create_dataloaders(
+        split["train_tensor"], batch_size=config.BATCH_SIZE
+    )
 
-        # Create DataLoaders
-        train_loader = create_dataloaders(
-            split["train_tensor"], batch_size=config.BATCH_SIZE
-        )
+    # Initialize model
+    gene_dim = data.shape[-1]
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        # Initialize model
-        gene_dim = data.shape[-1]
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model_seed = random.randint(0, 99999) # random seed for model weight initialization
+    torch.manual_seed(model_seed)
 
-        model_seed = random.randint(0, 99999) # random seed for model weight initialization
-        torch.manual_seed(model_seed)
+    model = SplitVAE(gene_dim=gene_dim, embedding_dim=config.EMBEDDING_DIM).to(device)
+    print(f"Running on device: {device}")
 
-        model = SplitVAE(gene_dim=gene_dim, embedding_dim=config.EMBEDDING_DIM).to(device)
-        print(f"Running on device: {device}")
+    # Train model
+    print("Training model...")
+    trained_model = train_model(model, train_loader, device, num_epochs=config.NUM_EPOCHS,
+                                learning_rate=config.LEARNING_RATE, name=config.NAME, lambda_weight=config.LAMBDA_WEIGHT, weight_decay=config.WEIGHT_DACAY)
+    
+    # Save model
+    trained_model_path = os.path.join(out_dir, f"split_autoencoder.pt")
+    torch.save(trained_model.state_dict(), trained_model_path)
+    print(f"Model saved to {trained_model_path}")
 
-        # Train model
-        print("Training model...")
-        trained_model = train_model(model, train_loader, device, num_epochs=config.NUM_EPOCHS,
-                                    learning_rate=config.LEARNING_RATE, name=config.NAME, lambda_weight=config.LAMBDA_WEIGHT, weight_decay=config.WEIGHT_DACAY)
+    # Save tensor of the embddings of the test set
+    test_tensor = split["test_tensor"]
+    trained_model.eval()
+    with torch.no_grad():  # Disable gradients for inference
+        test_tensor_embeddings, _, _, _ = trained_model.forward(test_tensor.to(device))   
+        test_tensor_embeddings = test_tensor_embeddings.cpu().detach().numpy()
+
+        # Average over the sample dimension (axis=0)
+        # Shape: (samples, bacteria, embedding_dim) -> (bacteria, embedding_dim)
+        bacteria_embeddings = np.mean(test_tensor_embeddings, axis=0)
         
-        # Save model
-        trained_model_path = os.path.join(out_dir, f"split_autoencoder.pt")
-        torch.save(trained_model.state_dict(), trained_model_path)
-        print(f"Model saved to {trained_model_path}")
+        #rel_abundance_path = config.REL_ABUNDANCE_PATH
+        #test_set_path = config.BACTERIA_LIST_EXPANDED_PATH
+        #bacteria_embeddings = cal_embedding(test_tensor_embeddings, rel_abundance_path, test_set_path)
+        # Extract the first half embedding (bacteria representation)
+        bacteria_embeddings = bacteria_embeddings[:, :config.EMBEDDING_DIM//2]
 
-        # Save tensor of the embddings of the test set
-        test_tensor = split["test_tensor"]
-        trained_model.eval()
-        with torch.no_grad():  # Disable gradients for inference
-            test_tensor_embeddings, _, _, _ = trained_model.forward(test_tensor.to(device))   
-            test_tensor_embeddings = test_tensor_embeddings.cpu().detach().numpy()
-
-            # Average over the sample dimension (axis=0)
-            # Shape: (samples, bacteria, embedding_dim) -> (bacteria, embedding_dim)
-            # (optional) bacteria_embeddings = np.mean(test_tensor_embeddings, axis=0)
-            
-            rel_abundance_path = config.REL_ABUNDANCE_PATH
-            test_set_path = config.BACTERIA_LIST_EXPANDED_PATH
-            bacteria_embeddings = cal_embedding(test_tensor_embeddings, rel_abundance_path, test_set_path)
-            # Extract the first half embedding (bacteria representation)
-            bacteria_embeddings = bacteria_embeddings[:, :config.EMBEDDING_DIM//2]
-
-            test_tensor_embeddings_path = os.path.join(out_dir, "test_tensor_embeddings.npy")
-            np.save(test_tensor_embeddings_path, bacteria_embeddings)
+        test_tensor_embeddings_path = os.path.join(out_dir, "test_tensor_embeddings.npy")
+        np.save(test_tensor_embeddings_path, bacteria_embeddings)
 
 if __name__ == "__main__":
-    main()
+main()
